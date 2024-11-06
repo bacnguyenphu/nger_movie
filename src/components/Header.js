5dbcda06280ac2ddee8b26a9ddddf7d7a5c5cc5d
bacnguyen <bac03906@gmail.com>
2024-11-06 15:48:24 +0700

    low

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { RiMovie2Line } from "react-icons/ri";
 import { FiSearch } from "react-icons/fi";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
 import { useRef, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom"
 import { FaAngleDown } from "react-icons/fa6";
@@ -9,7 +10,7 @@ import TippyMovieGenre from "./TippyMovieGenre";
 import TippyCountries from "./TippyCountries";
 
 
-function Header({ menu, scrollToTop, conutries, movieGenre, onClickMovieGenre,backToHomePage }) {
+function Header({ menu, scrollToTop, conutries, movieGenre, onClickMovieGenre, backToHomePage }) {
 
     const search = useRef()
     const input = useRef()
@@ -18,7 +19,7 @@ function Header({ menu, scrollToTop, conutries, movieGenre, onClickMovieGenre,ba
 
     const showInput = () => {
         if (search.current) {
-            search.current.classList.add('w-[300px]'); // Sử dụng toggle để mở/đóng
+            search.current.classList.add('lg:w-[300px]'); // Sử dụng toggle để mở/đóng
             input.current.focus()
         }
     };
@@ -31,16 +32,17 @@ function Header({ menu, scrollToTop, conutries, movieGenre, onClickMovieGenre,ba
         setValueInput('')
     }
 
-    const handlePressEnter = (e)=>{
-        if(e.keyCode===13){
+    const handlePressEnter = (e) => {
+        if (e.keyCode === 13) {
             handleSubmitSearch()
         }
     }
 
     const hiddenInput = () => {
         if (search.current) {
-            search.current.classList.remove('w-[300px]'); // Sử dụng toggle để mở/đóng
+            search.current.classList.remove('lg:w-[300px]'); // Sử dụng toggle để mở/đóng
             input.current.focus()
+            input.current.blur()
         }
     }
 
@@ -55,75 +57,85 @@ function Header({ menu, scrollToTop, conutries, movieGenre, onClickMovieGenre,ba
     //         .trim(); // Xóa khoảng trắng ở đầu và cuối chuỗi
     // };
 
-    const activeStyle = 'relative after:absolute after:w-full after:h-1 after:bg-blue-500 after:bottom-0 after:left-0 after:rounded-xl text-blue-500'
+    const activeStyle = 'relative lg:after:absolute lg:after:w-full lg:after:h-1 lg:after:bg-blue-500 lg:after:bottom-0 lg:after:left-0 lg:after:rounded-xl text-blue-500'
 
     return (
         <div className="px-2 lg:px-4 xl:px-28 h-[80px] flex items-center fixed bg-black-fade-top z-[100] w-full">
             <div className=" flex items-center w-full">
                 <div className="logo lg:w-1/4 flex items-center cursor-pointer"
-                    onClick={()=>{backToHomePage()}}
+                    onClick={() => { backToHomePage() }}
                 >
                     <RiMovie2Line size={'3rem'} color="#2563EB" />
                     <span className="text-3xl font-bold font-serif text-blue-600">NgerMovies</span>
                 </div>
 
-                <div className="xl:text-[17px] lg:flex text-[14px] hidden w-2/4 items-center justify-between font-semibold text-white">
-                    {/* <div className="cursor-pointer">Trang chủ</div>
+                <div className="bg-[#1A191F] absolute w-[95%] left-0 lg:w-2/4 lg:bg-transparent top-[80px] lg:top-0 lg:relative lg:block z-50">
+                    <div className="xl:text-[17px] lg:flex lg:text-[14px] text-2xl items-center justify-between font-semibold text-white relative">
+                        {/* <div className="cursor-pointer">Trang chủ</div>
                     <div className="cursor-pointer">Phim bộ</div>
                     <div className="cursor-pointer">Phim lẻ</div>
                     <div className="cursor-pointer">TV Show</div>
                     <div className="cursor-pointer">Hoạt hình</div> */}
-                    {/*  */}
-                    {/* <div className="cursor-pointer flex items-center">
+                        {/*  */}
+                        {/* <div className="cursor-pointer flex items-center">
                         Quốc gia
                         <span className="ml-1"><FaAngleDown /></span>
                     </div> */}
-                    {menu.map((item, index) => {
-                        return (
-                            <NavLink
-                                key={index}
-                                to={item.path}
-                                className={({ isActive }) => {
-                                    return isActive ? activeStyle : ''
-                                }}
-                            >
-                                <div className="cursor-pointer hover:text-blue-500" onClick={() => scrollToTop()}>
-                                    {item.text}
-                                </div>
-                            </NavLink>
-                        )
-                    })}
+                        {menu.map((item, index) => {
+                            return (
+                                <NavLink
+                                    key={index}
+                                    to={item.path}
+                                    className={({ isActive }) => {
+                                        return isActive ? activeStyle : ''
+                                    }}
+                                >
+                                    <div className="cursor-pointer py-4 lg:py-0 pl-4 lg:pl-0 hover:text-blue-500">
+                                        <span className="" onClick={() => scrollToTop()}>
+                                            {item.text}
+                                        </span>
+                                    </div>
+                                </NavLink>
+                            )
+                        })}
 
-                    <Tippy
-                        content={<TippyMovieGenre movieGenre={movieGenre} onClickMovieGenre={onClickMovieGenre} />}
-                        interactive={true}
-                        placement="bottom-end"
-                        animation="shift-away"
-                    >
-                        <div className="cursor-pointer flex items-center">
-                            Thể loại
-                            <span className="ml-1"><FaAngleDown /></span>
-                        </div>
-                    </Tippy>
+                        <Tippy
+                            content={<TippyMovieGenre movieGenre={movieGenre} onClickMovieGenre={onClickMovieGenre} />}
+                            interactive={true}
+                            placement="bottom-end"
+                            animation="shift-away"
+                            maxWidth={''}
+                            hideOnClick={true}
+                        >
+                            <div className="cursor-pointer flex items-center py-4 lg:py-0 pl-4 lg:pl-0">
+                                Thể loại
+                                <span className="ml-1"><FaAngleDown /></span>
+                            </div>
+                        </Tippy>
 
-                    <Tippy
-                        content={<TippyCountries conutries={conutries} onClickMovieGenre={onClickMovieGenre} />}
-                        interactive={true}
-                        animation="shift-away"
-                        placement="bottom-end"
-                    >
-                        <div className="cursor-pointer flex items-center">
-                            Quốc gia
-                            <span className="ml-1"><FaAngleDown /></span>
-                        </div>
-                    </Tippy>
+                        <Tippy
+                            content={<TippyCountries conutries={conutries} onClickMovieGenre={onClickMovieGenre} />}
+                            interactive={true}
+                            animation="shift-away"
+                            placement="bottom-end"
+                            maxWidth={''}
+                        >
+                            <div className="cursor-pointer flex items-center  py-4 lg:py-0 pl-4 lg:pl-0">
+                                Quốc gia
+                                <span className="ml-1"><FaAngleDown /></span>
+                            </div>
+                        </Tippy>
 
+                    </div>
 
+                    <div className="absolute top-10 -right-6 lg:hidden">
+                        <IoIosArrowDroprightCircle size={'3rem'} color="#2563EB" />
+                    </div>
                 </div>
 
                 <div className="lg:w-1/4 flex justify-end w-full">
                     <div
-                        className="input-search bg-white h-10 flex items-center rounded-full size-[40px] overflow-hidden transition-all duration-500 ease-in-out relative"
+                        className="input-search bg-white h-10 flex items-center rounded-full w-[300px] lg:size-[40px] overflow-hidden transition-all duration-500 ease-in-out relative"
                         ref={search}
                         onClick={() => showInput()}
                         onBlur={() => hiddenInput()}
@@ -132,7 +144,7 @@ function Header({ menu, scrollToTop, conutries, movieGenre, onClickMovieGenre,ba
                             className="w-full h-full outline-none border-none pl-2 pr-11"
                             ref={input}
                             onChange={(e) => { setValueInput(e.target.value) }}
-                            onKeyDown={(e)=>{handlePressEnter(e)}}
+                            onKeyDown={(e) => { handlePressEnter(e) }}
                         />
                         <span
                             className="px-2 cursor-pointer absolute right-0 size-[40px] flex items-center"
--- src/components/TippyCountries.js
+++ src/components/TippyCountries.js
@@ -1,6 +1,6 @@
 function TippyCountries({conutries,onClickMovieGenre}) {
     return ( 
-        <div className="rounded-md overflow-hidden w-full bg-[#17161B] font-light text-gray-500 p-5 text-[14px] flex flex-wrap justify-between gap-y-3 animate-height-down duration-200 after:h-[3px] after:w-full after:absolute after:bg-blue-500 after:left-0 after:top-0 after:rounded-t-md">
+        <div className=" rounded-md overflow-hidden lg:w-full bg-[#17161B] font-light text-gray-500 p-5 text-[14px] flex flex-wrap justify-between gap-y-3 animate-height-down duration-200 after:h-[3px] after:w-full after:absolute after:bg-blue-500 after:left-0 after:top-0 after:rounded-t-md">
             <div className="triangle-up absolute -top-[9px] right-6"></div>
             {conutries && conutries.length > 0 &&
                 conutries.map((item,index) => {
